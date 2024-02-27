cdfbaf2e5a08bfb03eecd3520da9b920c59f9b06
John Tran <trung.14112002@gmail.com>
2024-02-27 11:30:03 +1100

    Change Clayton to Caulfield

---
+++ p1.py
@@ -0,0 +1,16 @@
+# Program prog_01.py
+# Used to practice some Git operations
+# Make changes in this file and manage changes in Git
+
+def hw_001():
+	
+	print('Hello World\n')
+
+def hm_001():
+
+	print('Welcome to Monash University, Clayton, Victoria, Australia\n')	
+	print('Bye for now!\n')
+
+
+hw_001()
+hm_001()
